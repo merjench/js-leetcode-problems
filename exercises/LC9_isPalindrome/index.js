632d83b02ccbe2e652f22c44b6c55b066b8be911
Merjen Charyyeva <merjen.chary@gmail.com>
2020-02-25 22:53:42 -0500

    is palindrome?

--- exercises/LC9_isPalindrome/index.js
+++ exercises/LC9_isPalindrome/index.js
@@ -6,6 +6,23 @@
 // function isPalindrome(-121) --> false
 // function isPalindrome(10) --> false
 
-function isPalindrome(x) {}
+// mirror image of other half
+// create left and right pointer
+
+function isPalindrome(s) {
+  s = s.toLowerCase().replace(/[\W_]/g, "");
+
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
