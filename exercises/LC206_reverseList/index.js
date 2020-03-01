ea1ee186e5b5795188878100eda5de3235b5a8fc
Merjen Charyyeva <merjen.chary@gmail.com>
2020-02-29 23:27:29 -0500

    reverse linked list = LC206

--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,3 +1,18 @@
-const reverseList = head => {};
+const reverseList = head => {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let temp = current.next;
+
+    // change direction
+    current.next = prev;
+
+    // move prev and current up by 1
+    prev = current;
+    current = temp;
+  }
+  return prev;
+};
 
 module.exports = reverseList;
